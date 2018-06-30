04d8dbd35eb6404dceebdd0c102f2fda004cfb9e
Ricardo Rivera <rick_rivera662@yahoo.com>
2018-06-30 15:11:51 -0700

    feat: Add Player functionality
    Add player functionality; player is now drawn on screen and responds to arrow key presses. Additionally, Enemy class implementation was changed to ES6 implementation.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,91 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy{
+    /**
+     * @description Constructor.
+     */
+    constructor(){
+        // The image for our enemies.
+        this.sprite = "images/enemy-bug.png";
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    /**
+     * @description Update the enemy's position.
+     * @param dt A time delta between ticks.
+     */
+    update(dt){
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    /**
+     * @description Draw the enemy on the screen.
+     */
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+class Player {
+    /**
+     * @description Constructor.
+     */
+    constructor(){
+        this.player = "images/char-boy.png";
+        this.x = 2;
+        this.y = 4.5;
+    }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    /**
+     * @description Updates player data.
+     */
+    update(){
+    }
+
+    /**
+     * @description Draws player on the screen.
+     */
+    render(){
+        // Draw player in the middle of the block.
+        ctx.drawImage(Resources.get(this.player), this.x * 101, this.y * 83);
+    }
+
+    /**
+     * Handles arrow key presses. Updates player position accordingly.
+     * @param keyCode The key code for the key that was pressed.
+     */
+    handleInput(keyCode){
+        switch(keyCode){
+            case "up":
+            this.y--;
+            break;
+            case "down":
+            this.y++;
+            break;
+            case "left":
+            this.x--;
+            break;
+            case "right":
+            this.x++;
+            break;
+        }
+    }
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+// Listen for key presses and send the keys to the
+// Player.handleInput() method.
+document.addEventListener("keyup", function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+let allEnemies = [new Enemy()];
+let player = new Player();
+
+
